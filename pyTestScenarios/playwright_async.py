b8baecb98fc8a41ce434ff3ca03280ca33f49af6
Lokesh Jammi <Lokesh.Jammi@movate.com>
2026-02-11 10:53:07 +0530

    sync and async code commit

---
+++ pyTestScenarios/playwright_async.py
@@ -0,0 +1,13 @@
+from playwright.async_api import async_playwright
+import asyncio
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        await page.goto("https://www.google.com")
+        title = await page.title()
+        print(title)
+        await browser.close()
+
+asyncio.run(main())
